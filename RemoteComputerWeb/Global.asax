7483c19dbc7a6782fcb203f2dd427083e562a613
Mehdi Naseri <naseri@outlook.com>
2019-05-20 16:33:10 -0400

    Add project files.

---
+++ RemoteComputerWeb/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="RemoteComputerWeb.MvcApplication" Language="C#" %>
---
+++ RemoteComputerWeb/ViewModel/ActiveDirectoryComputerViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace RemoteComputerWeb.ViewModel
+{
+    public class ActiveDirectoryComputerViewModel
+    {
+        public String SamAccountName { get; set; }
+        public String DisplayName { get; set; }
+        public String Name { get; set; }
+        public String Description { get; set; }
+        public Boolean? Enabled { get; set; }
+        //public DateTime? LastLogon { get; set; }
+        public string LastLogon { get; set; }
+    }
+}
